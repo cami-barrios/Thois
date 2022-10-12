da7b4939b1af0e0ba17e523a1ee2f7488240e65c
Alejandro-Rmz <115659117+Alejandro-Rmz@users.noreply.github.com>
2022-10-12 17:23:05 -0500

    Create vld_login.php

---
+++ vld_login.php
@@ -0,0 +1,16 @@
+<?php
+
+$correo = $_POST['correo'];
+$contrasena = $_POST['contrasena'];
+
+$conex = mysqli_connect("localhost","root","","thois");
+
+$consult = "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contrasena'";
+
+$resultado = mysqli_query($conex,$consult);
+
+if($resultado==true){
+    echo "location: index.php";
+}
+
+?>
