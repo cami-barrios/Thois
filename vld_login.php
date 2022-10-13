3f46922ddf7a5130cdae6b0a7a0ae525e62f4897
Alejandro-Rmz <115659117+Alejandro-Rmz@users.noreply.github.com>
2022-10-13 10:05:03 -0500

    Update vld_login.php

--- vld_login.php
+++ vld_login.php
@@ -10,7 +10,7 @@ $consult = "SELECT * FROM usuarios WHERE correo='$correo' and contrasena='$contr
 $resultado = mysqli_query($conex,$consult);
 
 if($resultado==true){
-    header("location: index.php");
+    header("location: index.php")
 }
 
 ?>
