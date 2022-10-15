186f15268934393f6e579b888a147cdb802b8aa5
Alejandro-Rmz <115659117+Alejandro-Rmz@users.noreply.github.com>
2022-10-15 17:55:59 -0500

    Update vald-prod.php

--- vald-prod.php
+++ vald-prod.php
@@ -10,29 +10,35 @@ if(isset($_POST['submit'])){
         $descripcion = $_POST['descprod'];
     }
 
+    $errores = array();
+
     if(empty($ref)){
-        echo "<p class='error'>*Referencia esta vacio</p>";
+        $errores[] =  "<p class='error'>*Referencia esta vacio</p>";
     }
 
     if (empty($nombreprod)) {
-        echo "<p class='error'>*El nombre esta vacio</p>";
+        $errores[] = "<p class='error'>*El nombre esta vacio</p>";
     }else if(is_numeric($nombreprod)){
-        echo "<p class='error'>*En el nombre debe ser texto</p>";
+        $errores[] = "<p class='error'>*En el nombre debe ser texto</p>";
     }
 
     if(empty($cantidad)){
-        echo "<p class='error'>*La cantidad esta vacia</p>";
+        $errores[] = "<p class='error'>*La cantidad esta vacia</p>";
     }else if(!is_numeric($cantidad)){
-        echo "<p class='error'>*La cantidad debe ser numerica</p>";
+        $errores[] = "<p class='error'>*La cantidad debe ser numerica</p>";
     }
 
     if(empty($precio)){
-        echo "<p class='error'>*El precio esta vacio</p>";
+        $errores[] = "<p class='error'>*El precio esta vacio</p>";
     }else if(!is_numeric($precio)){
-        echo "<p class='error'>*El precio debe ser numerico</p>";
+        $errores[] = "<p class='error'>*El precio debe ser numerico</p>";
     }
 
-    if(!empty($ref) and !empty($nombreprod) and !empty($cantidad) and !empty($precio)){
+    for($i = 0;$i < count($errores); $i++){
+        echo $errores[$i];
+    }
+
+    if(!empty($ref) and !empty($nombreprod) and !empty($cantidad) and !empty($precio) && count($errores)==0){
         include("insert-prod.php");
     }
 
